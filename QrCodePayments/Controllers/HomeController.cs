db1cc0f9308f7e631f6f4da95dc6202f7a652b68
Ankur N Bhavsar <akon1113@gmail.com>
2022-09-01 13:47:46 +0100

    Add project files.

---
+++ QrCodePayments/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Razorpay.Api;
+
+namespace QrCodePayments.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            RazorpayClient RC = new RazorpayClient("rzp_test_4cMvJ6ZpAWMdsS", "dXbpU75yzn3IQPoKKkjEOf44");
+
+            Dictionary<string, object> attributes = new Dictionary<string, object>();
+            //attributes.Add("type", "upi_qr");
+           // attributes.Add("fixed_amount", "true");
+            List<QrCode> Data = new List<QrCode>();
+            try
+            {
+                Data = RC.QrCode.fetchAll();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            List<Models.Root> rt = new List<Models.Root>();
+            if (Data.Count > 0)
+            {
+                foreach (var item in Data)
+                {
+                    Models.Root tt = new Models.Root();
+
+                    foreach (var item1 in item.Attributes)
+                    {
+
+                        string typ = item1.Name;
+                        switch (typ)
+                        {
+                            case nameof(tt.id):
+                                tt.id = item1.Value;
+                                break;
+
+                            case nameof(tt.notes):
+                                Dictionary<string, string> nt = new Dictionary<string, string>();
+                                foreach (var ntt in item1.Value)
+                                {
+                                    string key = ntt.Name;
+                                    string val = ntt.Value;
+                                    nt.Add(key, val);
+                                }
+                                tt.notes = nt;
+                                break;
+
+                            case nameof(tt.fixed_amount):
+                                tt.fixed_amount = item1.Value;
+                                break;
+                            case nameof(tt.payments_amount_received):
+                                tt.payments_amount_received = item1.Value;
+                                break;
+                            case nameof(tt.payments_count_received):
+                                tt.payments_count_received = item1.Value;
+                                break;
+                            case nameof(tt.type):
+                                tt.type = item1.Value;
+                                break;
+                            case nameof(tt.payment_amount):
+                                tt.payment_amount = item1.Value;
+                                break;
+                        }
+
+                    }
+                    rt.Add(tt);
+
+                }
+            }
+
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ QrCodePayments/Models/Root.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QrCodePayments.Models
+{
+    public class Root
+    {
+        public string id { get; set; }
+        public string entity { get; set; }
+        public int created_at { get; set; }
+        public string name { get; set; }
+        public string usage { get; set; }
+        public string type { get; set; }
+        public string image_url { get; set; }
+        public string payment_amount { get; set; }
+        public string status { get; set; }
+        public string description { get; set; }
+        public bool fixed_amount { get; set; }
+        public string payments_amount_received { get; set; }
+        public string payments_count_received { get; set; }
+        public Dictionary<string,string> notes { get; set; }
+        public string customer_id { get; set; }
+        public int close_by { get; set; }
+        public int closed_at { get; set; }
+        public string close_reason { get; set; }
+    }
+   
+}
